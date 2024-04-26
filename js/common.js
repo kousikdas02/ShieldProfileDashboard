83ad75525f576b180ba20b7ecf7b75232ad60b0c
Kousik Das <kousikdas02@gmail.com>
2024-04-27 01:48:56 +0530

    edit profile page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
+   // file upload preview
+// function readURL(input) {
+
+//     if (input.files && input.files[0]) {
+//       var reader = new FileReader();
+     
+//       reader.onload = function (e) {
+  
+//         // Use the input element's name attrbute to select and
+//         // update the image element with matching id
+//         $('#' + input.name).attr('src', e.target.result);
+
+        
+//       }
+//       reader.readAsDataURL(input.files[0]);
+//     }
+//   }
+
 jQuery(document).ready(function ($) {
     // document start
 
@@ -79,6 +97,64 @@ jQuery(document).ready(function ($) {
     });
 
 
+    // tooltip
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $(".cmn_single_select").select2({
+        placeholder: "Select an Option",
+        allowClear: true
+    });
+    $(".cmn_multi_select").select2({
+        placeholder: "Select an Option",
+
+    });
+
+    $('.upload_preview figure img').click(function() {
+        // clicks on the respective input
+        $(this).parent().parent().parent().find('input').click();
+      });
+      // creates an onchange event listener for all the inputs
+      $('.fileUpload_box_cntent input').change(function() {
+        // holds scope for this input
+        var selector = $(this);
+        // selector[0] references the direct element without jQuery since we need to access the files object inside of it
+        if (selector[0].files && selector[0].files[0]) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            selector.parent().next().find( "img" ).attr('src', e.target.result);
+            selector.parent().next().removeClass("hide");
+            selector.parent().addClass("hide");
+          }
+          reader.readAsDataURL(selector[0].files[0]);
+        }else{
+            selector.parent().removeClass("hide");
+        }   
+      });
+      $('.upload_preview_close').click(function() {
+        $(this).parent().prev().removeClass("hide");
+        $(this).next().find("img").attr('src', '');
+        $(this).parent().addClass("hide");
+        
+      });
+   
+
+    //   use image preview
+    $('#userImgUpload').change(function() {
+        var selector = $(this);
+        if (selector[0].files && selector[0].files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              selector.parent().parent().find( "img" ).attr('src', e.target.result);
+            }
+            reader.readAsDataURL(selector[0].files[0]);
+          }
+    });
+
+     $('#userImgDelete').click(function() {
+        $(this).parent().prev().find( "img" ).attr('src', "../images/user-placeholder.png");
+     });
     // document end
 
 })
